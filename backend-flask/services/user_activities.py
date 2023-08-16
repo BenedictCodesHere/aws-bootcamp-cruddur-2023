73278874fd3805f8472db42d7fe467c188475a56
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-08-16 00:06:23 +0000

    added try and finally to user_activities

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -3,30 +3,35 @@ from aws_xray_sdk.core import xray_recorder
 
 class UserActivities:
     def run(user_handle):
-        model = {
-            'errors': None,
-            'data': None
-        }
-
-        now = datetime.now(timezone.utc).astimezone()
-        if user_handle == None or len(user_handle) < 1:
-            model['errors'] = ['blank_user_handle']
-        else:
-            now = datetime.now()
-            results = [{
-                'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-                'handle': 'Andrew Brown',
-                'message': 'Cloud is fun!',
-                'created_at': (now - timedelta(days=1)).isoformat(),
-                'expires_at': (now + timedelta(days=31)).isoformat()
-            }]
-            model['data'] = results
-            subsegment = xray_recorder.begin_subsegment('mock-data-subsegment')
-            data_dict_two = {
-                "now": now.isoformat(), 
-                "results-size": len(model['data'])
+        try:
+            model = {
+                'errors': None,
+                'data': None
             }
-            subsegment.put_metadata('key', data_dict_two, 'namespace')
+
+            now = datetime.now(timezone.utc).astimezone()
+            if user_handle == None or len(user_handle) < 1:
+                model['errors'] = ['blank_user_handle']
+            else:
+                now = datetime.now()
+                results = [{
+                    'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+                    'handle': 'Andrew Brown',
+                    'message': 'Cloud is fun!',
+                    'created_at': (now - timedelta(days=1)).isoformat(),
+                    'expires_at': (now + timedelta(days=31)).isoformat()
+                }]
+                model['data'] = results
+
+                subsegment = xray_recorder.begin_subsegment('mock-data-subsegment')
+                data_dict_two = {
+                    "now": now.isoformat(), 
+                    "results-size": len(model['data'])
+                }
+
+                subsegment.put_metadata('key', data_dict_two, 'namespace')
+                xray_recorder.end_subsegment()
+        finally:
             xray_recorder.end_subsegment()
         return model  
    
