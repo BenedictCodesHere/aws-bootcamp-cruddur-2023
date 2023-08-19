c16cb879d512acbadd10bb358b1ad5706677d750
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-08-19 14:28:35 +0000

    added cors and header auth changes

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -86,8 +86,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -158,6 +158,10 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 @xray_recorder.capture('activities_home')
 def data_home():
+  print('AUTH HEADER----')
+  print(
+    request.headers.get('Authorization')
+  )
  
   data = HomeActivities.run()  # logger=LOGGER in brackets
   return data, 200
