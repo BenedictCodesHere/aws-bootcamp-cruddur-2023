7a3449494b6928db3643d48bd0e45d5a428f0809
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-08-30 00:35:02 +0000

    added user_uuid as correct column into schema

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,7 @@ VALUES
   ('Andrew Brown', 'andrewbrown' ,'MOCK'),
   ('Andrew Bayko', 'bayko' ,'MOCK');
 
-INSERT INTO public.activities (uuid, message, expires_at)
+INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
     (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
