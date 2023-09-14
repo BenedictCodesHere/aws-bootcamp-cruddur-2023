44412a39830d39dad94802a1e70cf4d15cf8929e
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-09-14 16:03:46 +0000

    adding create new message thread

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,7 +1,9 @@
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('Kestrel Jack', 'KestrelBlaster' ,'cloudagemthrowaway5@gmail.com', 'MOCK'),
-  ('Horace Turnbull', 'turninghorace' ,'timberbertim88@gmail.com','MOCK');
+  ('Horace Turnbull', 'turninghorace' ,'timberbertim88@gmail.com','MOCK'),
+  ('Neville Tracer', 'tracerneville' ,'aweotgwe47895hbfiwdu@hotmail.com','MOCK');
+  
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
