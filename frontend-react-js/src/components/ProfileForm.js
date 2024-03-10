abc3f00e13d2d0f29cefc81c385eb297bbc01a9a
Benedict McElroy <codingwithbenedict99@gmail.com>
2024-03-10 23:09:17 +0000

    refactored the error handling for fetch requests, refactored Requests into lib,created FormErrorItem and FormErrors, updated the sql for the home and show activities

--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -2,6 +2,8 @@ import './ProfileForm.css';
 import React from "react";
 import process from 'process';
 import {getAccessToken} from 'lib/CheckAuth';
+import {put} from 'lib/Requests';
+import FormErrors from 'components/FormErrors';
 
 export default function ProfileForm(props) {
   const [bio, setBio] = React.useState('');
@@ -16,7 +18,6 @@ export default function ProfileForm(props) {
     console.log('ext',extension)
     try {
       const gateway_url = `${process.env.REACT_APP_API_GATEWAY_ENDPOINT_URL}/avatars/key_upload`
-      console.log(gateway_url);
       await getAccessToken()
       const access_token = localStorage.getItem("access_token")
       const json = {
@@ -34,8 +35,7 @@ export default function ProfileForm(props) {
       })
       let data = await res.json();
       if (res.status === 200) {
-        console.log(data);
-        return data.url;
+        return data.url
       } else {
         console.log(res)
       }
@@ -49,7 +49,7 @@ export default function ProfileForm(props) {
     const filename = file.name
     const size = file.size
     const type = file.type
-    // const preview_image_url = URL.createObjectURL(file)
+    //const preview_image_url = URL.createObjectURL(file)
     console.log(filename,size,type)
     const fileparts = filename.split('.')
     const extension = fileparts[fileparts.length-1]
@@ -62,9 +62,8 @@ export default function ProfileForm(props) {
         headers: {
           'Content-Type': type
       }})
-      // let data = await res.json();
       if (res.status === 200) {
-       
+        
       } else {
         console.log(res)
       }
@@ -75,33 +74,16 @@ export default function ProfileForm(props) {
 
   const onsubmit = async (event) => {
     event.preventDefault();
-    try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/profile/update`
-      await getAccessToken()
-      const access_token = localStorage.getItem("access_token")
-      const res = await fetch(backend_url, {
-        method: "POST",
-        headers: {
-          'Authorization': `Bearer ${access_token}`,
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          bio: bio,
-          display_name: displayName
-        }),
-      });
-      let data = await res.json();
-      if (res.status === 200) {
-        // setBio('')
-        // setDisplayName('')
-        props.setPopped(false)
-      } else {
-        console.log(res)
-      }
-    } catch (err) {
-      console.log(err);
+    const url = `${process.env.REACT_APP_BACKEND_URL}/api/profile/update`
+    const payload_data = {
+      bio: bio,
+      display_name: displayName
     }
+    put(url,payload_data,setErrors,function(data){
+      setBio(null)
+      setDisplayName(null)
+      props.setPopped(false)
+    })
   }
 
   const bio_onchange = (event) => {
@@ -132,8 +114,7 @@ export default function ProfileForm(props) {
             </div>
           </div>
           <div className="popup_content">
-            
-          <input type="file" name="avatarupload" onChange={s3upload} />
+            <input type="file" name="avatarupload" onChange={s3upload} />
 
             <div className="field display_name">
               <label>Display Name</label>
@@ -152,6 +133,7 @@ export default function ProfileForm(props) {
                 onChange={bio_onchange} 
               />
             </div>
+            <FormErrors errors={errors} />
           </div>
         </form>
       </div>
