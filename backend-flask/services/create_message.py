e17429aa9dbaaafc1ba6182683bb80ac2b4b9367
Benedict McElroy <codingwithbenedict99@gmail.com>
2024-03-11 13:36:07 +0000

    fixed receiver typo

--- backend-flask/services/create_message.py
+++ backend-flask/services/create_message.py
@@ -21,7 +21,7 @@ class CreateMessage:
 
     if (mode == "create"):
       if user_receiver_handle == None or len(user_receiver_handle) < 1:
-        model['errors'] = ['user_reciever_handle_blank']
+        model['errors'] = ['user_receiver_handle_blank']
 
     if message == None or len(message) < 1:
       model['errors'] = ['message_blank'] 
