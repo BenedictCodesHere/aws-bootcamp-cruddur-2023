9ad208263713c64d5bb26f7e7006179d3a1ee3a8
Benedict McElroy <codingwithbenedict99@gmail.com>
2024-03-14 22:28:43 +0000

    updating styles; testing build

--- frontend-react-js/src/components/ActivityContent.js
+++ frontend-react-js/src/components/ActivityContent.js
@@ -17,20 +17,24 @@ export default function ActivityContent(props) {
 
   return (
     <div className='activity_content_wrap'>
-      <Link className='activity_avatar'to={`/@`+props.activity.handle} ></Link>
+      
       <div className='activity_content'>
-        <div className='activity_meta'>
+        <div className="activity-avatar-and-meta-wrap">
+          <Link className='activity_avatar'to={`/@`+props.activity.handle} ></Link>
           <div className='activity_identity' >
-            <Link className='display_name' to={`/@`+props.activity.handle}>{props.activity.display_name}</Link>
-            <Link className="handle" to={`/@`+props.activity.handle}>@{props.activity.handle}</Link>
-          </div>{/* activity_identity */}
-          <div className='activity_times'>
-            <div className="created_at" title={format_datetime(props.activity.created_at)}>
-              <span className='ago'>{time_ago(props.activity.created_at)}</span> 
-            </div>
-            {expires_at}
-          </div>{/* activity_times */}
-        </div>{/* activity_meta */}
+                <Link className='display_name' to={`/@`+props.activity.handle}>{props.activity.display_name}</Link>
+                <Link className="handle" to={`/@`+props.activity.handle}>@{props.activity.handle}</Link>
+              </div>{/* activity_identity */}
+            <div className='activity_meta'>
+              
+              <div className='activity_times'>
+                <div className="created_at" title={format_datetime(props.activity.created_at)}>
+                  <span className='ago'>{time_ago(props.activity.created_at)}</span> 
+                </div>
+                {expires_at}
+              </div>{/* activity_times */}
+           </div>{/* activity_meta */}
+        </div>
         <div className="message">{props.activity.message}</div>
       </div>{/* activity_content */}
     </div>
