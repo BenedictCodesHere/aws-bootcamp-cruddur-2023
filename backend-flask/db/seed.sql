ed760da47d055e3dd0ef6432bd597509a1b6453d
Benedict McElroy <codingwithbenedict99@gmail.com>
2024-03-26 17:31:32 +0000

    updated seed data for further dev testing, added account id back in as variable substitution causing issues

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,20 +1,24 @@
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Kestrel Jack', 'KestrelBlaster' ,'cloudagemthrowaway5@gmail.com', 'MOCK'),
-  ('Horace Turnbull', 'turninghorace' ,'timberbertim88@gmail.com','MOCK'),
-  ('Neville Tracer', 'tracerneville' ,'idontoashteau@aufhwiewaeivcn.io','MOCK'),
-  ('Boris Yeltser', 'BorisYelt' ,'borisyelt@sharklasers.com','MOCK');
+  ('Devington Dev', 'Devington' ,'devington@sharklasers.com', 'MOCK'),
+  ('David Dev', 'DavidDev' ,'daviddev@sharklasers.com','MOCK'),
+  ('Darren Dev', 'DarrenDev' ,'darrendev@sharklasers.com','MOCK');
   
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'KestrelBlaster' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'Devington' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   ),
   (
-    (SELECT uuid from public.users WHERE users.handle = 'BorisYelt' LIMIT 1),
-    'I am the other!',
+    (SELECT uuid from public.users WHERE users.handle = 'DavidDev' LIMIT 1),
+    'I am the second dev user.',
+    current_timestamp + interval '10 day'
+  ),
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'DarrenDev' LIMIT 1),
+    'Guess that makes me the third wheel then.',
     current_timestamp + interval '10 day'
   );
